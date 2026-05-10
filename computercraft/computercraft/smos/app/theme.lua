6690df81922ad005a4cc9ae79c16b38af7b7a2d3
Sora-Rui <safaayar8@gmail.com>
2026-05-10 05:19:14 +0200

    Add files via upload

---
+++ computercraft/computercraft/smos/app/theme.lua
@@ -0,0 +1,17 @@
+local theme = {
+    brandName = "Sora's Magical OS",
+    subtitle = "Magisches Bordsystem",
+    shipName = "Luftschiff Morgenstern",
+    accent = colors.purple,
+    accentLight = colors.magenta,
+    text = colors.white,
+    muted = colors.lightGray,
+    shadow = colors.black,
+    warning = colors.red,
+    ok = colors.lime,
+    panel = colors.gray,
+    panelDark = colors.lightBlue,
+    credit = "Made by SoraRui_",
+}
+
+return theme
